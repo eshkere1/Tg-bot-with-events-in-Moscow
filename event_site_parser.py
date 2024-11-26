36f35398bc6597db77f3ddeb7d6b962f1e641f41
NadyaEvp <t_evp@mail.ru>
2024-11-26 21:52:07 +0300

    Получаем все мероприятия

--- event_site_parser.py
+++ event_site_parser.py
@@ -10,17 +10,27 @@ def get_name_url_events(url, response):
     tags_name_events = soup.find_all("a", class_="CjnHd y8A5E L_ilg Q8u7A R1IVp nFaMY")
 
     for tag in tags_name_events:
-        if tag["href"] != "/selection/":
+        if tag["href"] != "/selection/" and tag["href"] != "/msk/restaurants/?utm_campaign=headline&utm_medium=referral&utm_source=afisha":
             url_events.append(urljoin(url, tag["href"]))
             name_events.append(tag.text)
-
 
     return url_events[0:5], name_events[0:5]
 
 
-def get_all_events(urls):
-    pass
+def get_all_events(url_site, url):
+    url_events = []
+    response = requests.get(url)
+    response.raise_for_status()
 
+    soup = BeautifulSoup(response.text, "lxml")
+    card_events = soup.find_all('div', class_='oP17O')
+
+    for card in card_events:
+        element_url = card.find('div', class_='QsWic rIUFv').find('a', class_='CjnHd y8A5E Vrui1')['href']
+        url_event = urljoin(url_site, element_url)
+        url_events.append(url_event)
+
+    return url_events
 
 def main():
     url_site = "https://www.afisha.ru/"
@@ -28,9 +38,8 @@ def main():
     response.raise_for_status()
 
     url_events, name_events = get_name_url_events(url_site, response)
-    get_all_events(url_events)
-    print(url_events)
-    print(name_events)
+    url_event = get_all_events(url_site, url_events[0])
+    print(url_event)
 
 
 if "__main__" == __name__:
