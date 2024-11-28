662fff0160328fe1c7fbfb3f0b4ddda6033a23c1
Pavel <piv17032009@gmail.com>
2024-11-28 20:18:40 +0300

    Update get_weather.py

--- get_weather.py
+++ get_weather.py
@@ -1,36 +1,34 @@
 from meteostat import Point, Daily
 from datetime import datetime
+import numpy as np 
 
-def get_user_input(date_input="2024-11-30"):
+
+
+def get_weather_report(date_input):
+    latitude = 55.7522
+    longitude = 37.6156
     try:
         date = datetime.strptime(date_input, "%Y-%m-%d")
     except ValueError:
-        print("Неверный формат даты. Попробуйте снова.")
-    return latitude, longitude, date
-
-def fetch_weather_data(latitude, longitude, date):
+        return "Неверный формат даты. Используйте формат ГГГГ-ММ-ДД."
     location = Point(latitude, longitude)
-    data = Daily(location, date, date)
-    return data.fetch()
-
-def display_weather_data(data):
-    if not data.empty:
-        min_temp = data['tmin'].iloc[0]
-        max_temp = data['tmax'].iloc[0]
-        print(f"Минимальная температура: {min_temp} °C")
-        print(f"Максимальная температура: {max_temp} °C")
+    data = Daily(location, date, date).fetch()
+    if data.empty:
+        return "Данные о погоде не найдены для указанного местоположения и даты."
+    min_temp = data['tmin'].iloc[0]
+    max_temp = data['tmax'].iloc[0]
+    precipitation = data['prcp'].iloc[0]
+    result = f"Минимальная температура: {min_temp} °C\n"
+    result += f"Максимальная температура: {max_temp} °C\n"
+    if np.isnan(precipitation):
+        result += "Осадки не наблюдаются"
     else:
-        print("Данные о погоде не найдены для указанного местоположения и даты.")
-
-def main():
-    latitude, longitude, date = get_user_input()
-    if not all([latitude, longitude, date]):
-        return
+        result += f"Осадки: {precipitation} мм"
     
-    data = fetch_weather_data(latitude, longitude, date)
-    display_weather_data(data)
+    return result
+
 
 if __name__ == "__main__":
-    latitude = 55.7522
-    longitude = 37.6156
-    main()
+    date_input = "2024-11-30"  # Пример даты
+    report = get_weather_report(date_input)
+    print(report)
