c95d565b9170057efff2c0fef13231b0d1194226
NadyaEvp <t_evp@mail.ru>
2024-12-01 12:15:37 +0300

    Update get_weather.py

--- get_weather.py
+++ get_weather.py
@@ -1,7 +1,6 @@
 from meteostat import Point, Daily
 from datetime import datetime
-import numpy as np 
-
+import numpy as np
 
 
 def get_weather_report(date_input):
@@ -15,16 +14,16 @@ def get_weather_report(date_input):
     data = Daily(location, date, date).fetch()
     if data.empty:
         return "Данные о погоде не найдены для указанного местоположения и даты."
-    min_temp = data['tmin'].iloc[0]
-    max_temp = data['tmax'].iloc[0]
-    precipitation = data['prcp'].iloc[0]
+    min_temp = data["tmin"].iloc[0]
+    max_temp = data["tmax"].iloc[0]
+    precipitation = data["prcp"].iloc[0]
     result = f"Минимальная температура: {min_temp} °C\n"
     result += f"Максимальная температура: {max_temp} °C\n"
     if np.isnan(precipitation):
         result += "Осадки не наблюдаются"
     else:
         result += f"Осадки: {precipitation} мм"
-    
+
     return result
 
 
